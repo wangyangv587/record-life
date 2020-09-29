0a9d6824f0ceaa768e5991cf01350fd29eab39bb
杨旺 <yangwangv5876@163.com>
2020-09-29 17:27:54 +0800

    init

--- src/components/type-select/index.vue
+++ src/components/type-select/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="component">
-<!--    <input type="text" id="typeSelect" :placeholder="typePlaceHolder" @click="selectArea" v-model="typeName"-->
-<!--           class="select"/>-->
-<!--    <input type="button" value="×" class="inputIcon" @click="clearType"/>-->
-<!--    <div v-show="isShow">-->
-<!--      <ul class="ulClass">-->
-<!--        <li v-for="(item,index) in dataList" :key="index" @click="setArea(item)" style="list-style-type: none">-->
-<!--          {{item.typeName}}-->
-<!--        </li>-->
-<!--      </ul>-->
-<!--    </div>-->
+    <!--  <input type="text" id="typeSelect" :placeholder="typePlaceHolder" @click="selectArea" v-model="typeName"-->
+    <!--       class="select"/>-->
+    <!--  <input type="button" value="×" class="inputIcon" @click="clearType"/>-->
+    <!--  <div v-show="isShow">-->
+    <!--    <ul class="ulClass">-->
+    <!--    <li v-for="(item,index) in dataList" :key="index" @click="setArea(item)" style="list-style-type: none">-->
+    <!--      {{item.typeName}}-->
+    <!--    </li>-->
+    <!--    </ul>-->
+    <!--  </div>-->
     <el-select v-model="typeName" clearable placeholder="请选择" @change="changeType">
       <el-option
         v-for="item in dataList"
@@ -17,76 +17,74 @@
         :label="item.typeName"
         :value="item.typeId"
         @change="changeType1"
-        >
+      >
       </el-option>
     </el-select>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "index.vue",
-        data() {
-            return {
-                typePlaceHolder: "请选择类型",
-                isShow: false,
-                typeName: '',
-                typeId: '',
-                dataList: [
-                    {
-                        typeId: "1",
-                        typeName: "美食",
-                    }, {
-                        typeId: "2",
-                        typeName: "风景",
-                    }, {
-                        typeId: "3",
-                        typeName: "趣图",
-                    }, {
-                        typeId: "4",
-                        typeName: "YY",
-                    }
-                ]
-            };
-        },
-        methods: {
-            selectArea: function () {
-                this.isShow = !this.isShow;
-            },
-            setArea: function (item) {
-                this.typeName = item.typeName;
-                this.typeId = item.typeId;
-                console.log("shadow", "typeId", this.typeId);
-                console.log("shadow", "typeName", this.typeName);
-                this.isShow = false;
-            }, clearType: function () {
-                this.typeId = '';
-                this.typeName = '';
-            },
-            changeType:function (item) {
-                console.log(item);
-            },
-            changeType1:function (item) {
-                console.log(item);
-            }
-        },
-        props:{
-
-        },
-        created() {
-            console.log("lifecycle", "created");
-        }, mounted() {
-            console.log("lifecycle", "mounted");
-        }, beforeCreate() {
-            console.log("lifecycle", "beforeCreate");
-        }, beforeMount() {
-            console.log("lifecycle", "beforeMount");
-        }, deactivated() {
-            console.log("lifecycle", "deactivated");
-        }, destroyed() {
-            console.log("lifecycle", "destroyed");
-        }
+  export default {
+    name: 'index.vue',
+    data () {
+      return {
+        typePlaceHolder: '请选择类型',
+        isShow: false,
+        typeName: '',
+        typeId: '',
+        dataList: [
+          {
+            typeId: '1',
+            typeName: '美食',
+          }, {
+            typeId: '2',
+            typeName: '风景',
+          }, {
+            typeId: '3',
+            typeName: '趣图',
+          }, {
+            typeId: '4',
+            typeName: 'YY',
+          }
+        ]
+      }
+    },
+    methods: {
+      selectArea: function () {
+        this.isShow = !this.isShow
+      },
+      setArea: function (item) {
+        this.typeName = item.typeName
+        this.typeId = item.typeId
+        console.log('shadow', 'typeId', this.typeId)
+        console.log('shadow', 'typeName', this.typeName)
+        this.isShow = false
+      }, clearType: function () {
+        this.typeId = ''
+        this.typeName = ''
+      },
+      changeType: function (item) {
+        console.log(item)
+      },
+      changeType1: function (item) {
+        console.log(item)
+      }
+    },
+    props: {},
+    created () {
+      console.log('lifecycle', 'created')
+    }, mounted () {
+      console.log('lifecycle', 'mounted')
+    }, beforeCreate () {
+      console.log('lifecycle', 'beforeCreate')
+    }, beforeMount () {
+      console.log('lifecycle', 'beforeMount')
+    }, deactivated () {
+      console.log('lifecycle', 'deactivated')
+    }, destroyed () {
+      console.log('lifecycle', 'destroyed')
     }
+  }
 </script>
 
 <style scoped>
